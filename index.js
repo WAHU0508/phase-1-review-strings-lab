8f90e94b76b4e8dfff28fe90c2ab124ebb09469b
WAHU0508 <sharongikenye@gmail.com>
2024-06-25 20:31:20 +0300

    Review strings

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+// test-1: declare the variable currentUser
+const currentUser = "Wahu Gikenye";
+console.log(currentUser);
+
+//test-2: declare welcomeMessage 
+/*Concatenate strings using + operator
+const welcomeMessage = "Welcome to Flatbook, " + currentUser;
+*/
+//string interpolation.
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+//test-3: excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+//test-4: shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting);
+
